a707eec8819df5c2e736fc11636599327984b384
Krzysztof Bożek <90691419+Savetheprogram@users.noreply.github.com>
2023-03-30 21:42:08 +0200

    Added div container feature
    Div container will always be a square

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ divGenerator.addEventListener('click', () => {
 })
 
 function createGrid() {
-      const dimentions = prompt("jaki bok wariacie")
-    const grid = document.querySelector('.container')
-    for (let i=0; i<(dimentions*dimentions); i++) {
+      let dimensions = prompt("jaki bok wariacie (max 100)")
+    if (dimensions>100 || dimensions<0){
+    for (let i =dimensions; i>100 || i<0; i=dimensions ) {
+      let newDimensions = prompt("Podaj poprawną wartość (max 100)");
+        dimensions=newDimensions;
+    }}
+    const removeGrid = document.querySelector('.container')
+        for (let i=0; i<(dimensions*dimensions); i++) {
+            const removeGridContent = document.getElementsByClassName('sketch')
+            removeGridContent.remove()
+        }
 
+    const grid = document.querySelector('.container')
+    grid.style.height = +dimensions * 7 +"px"
+    grid.style.width = +dimensions * 7 +"px"
+    for (let i=0; i<(dimensions*dimensions); i++) {
         const gridContent = document.createElement('div')
         gridContent.classList.add('sketch')
         grid.appendChild(gridContent)
